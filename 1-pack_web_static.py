099743b79039c37a341962fe6bc568dd35dc4ca3
cheiy <gschege@gmail.com>
2023-05-06 23:16:24 +0000

    Pack web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''
+This script generates a .tgz archive from the contents of web_static folder
+'''
+
+
+from fabric.api import *
+import datetime
+
+
+def do_pack():
+    '''
+    Function compresses folder to .tgz
+    '''
+    now = datetime.datetime.now()
+    local('mkdir -p versions')
+    yr = str(now.year)
+    mn = str(now.month)
+    day = str(now.day)
+    H = str(now.hour)
+    M = str(now.minute)
+    S = str(now.second)
+    our_date = yr+mn+day+H+M+S
+    res = local("tar -cvzf  versions/web_static_"+our_date+".tgz web_static")
+    if res.failed is False:
+        fsize = local('ls -s versions/web_static_' + our_date + '.tgz',
+                      capture=True)
+        fsize = fsize.partition(" ")
+        fsize = fsize[0]
+        print("web_static packed: versions/web_static_" + our_date + ".tgz\
+              --> " + fsize + "Bytes")
