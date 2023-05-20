20533859db725e20b896df6106e2fb7ca317b1cb
cheiy <gschege@gmail.com>
2023-05-20 15:29:51 +0000

    if __name__ == __main__

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,9 +8,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    '''
-    Function returns the text
-    '''
-    return 'Hello HBNB!'
+if __name__ == "__main__":
+    @app.route('/', strict_slashes=False)
+    def hello_hbnb():
+        '''
+        Function returns the text
+        '''
+        return 'Hello HBNB!'
